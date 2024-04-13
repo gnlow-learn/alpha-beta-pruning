f1006b66cff8245755badcc0bb2e8d72268d5ae5
Gnlow <gnlow@protonmail.com>
2024-04-13 12:02:26 +0900

    nice

--- abPruning.ts
+++ abPruning.ts
@@ -29,17 +29,32 @@ const tree: Tree<number> = [
     ],
 ]
 
+const log =
+(depth: number, curr: number | string, alpha: number, beta: number) => {
+    const indent =
+        " " .repeat(10)
+            .repeat(4-depth)
+    const alphaS = String(alpha)
+        .replace("Infinity", "∞")
+        .padStart(2, " ")
+    const betaS = String(beta)
+        .replace("Infinity", "∞")
+        .padStart(2, " ")
+
+    console.log(indent, curr)
+    console.log(indent, `[${alphaS} , ${betaS} ]`)
+}
+
 const m =
 (alpha = -Infinity, beta = Infinity) =>
 (depth: number, isMax: boolean) =>
 (tree: number | Tree<number>): number => {
+    if (alpha >= beta) {
+        log(depth, "CUT", alpha, beta)
+        return isMax ? Infinity : -Infinity
+    }
     if (typeof tree == "number") {
-        console.log(
-            " ".repeat((4-depth)*4),
-            tree,
-            [alpha, beta],
-            alpha >= beta ? "CUT" : ""
-        )
+        log(depth, tree, alpha, beta)
         return tree
     } else {
         if (isMax) {
@@ -51,7 +66,7 @@ const m =
                 max = Math.max(max, curr)
             })
 
-            console.log(" ".repeat((4-depth)*4), max, [alpha, beta])
+            log(depth, max, alpha, beta)
             return max
         } else {
             let min = Infinity
@@ -62,7 +77,7 @@ const m =
                 min = Math.min(min, curr)
             })
 
-            console.log(" ".repeat((4-depth)*4), beta, [alpha, beta])
+            log(depth, min, alpha, beta)
             return min
         }
     }
